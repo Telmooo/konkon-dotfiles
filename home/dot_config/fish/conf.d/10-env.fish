1b575f1e2a13989fdeab3dba02155be20502d202
Telmooo <telmooo.dev@gmail.com>
2026-04-20 04:29:49 +0100

    feat: replace waybar with noctalia-shell
    Add fallback applications
    Changes to keybinds

--- home/dot_config/fish/conf.d/10-env.fish
+++ home/dot_config/fish/conf.d/10-env.fish
@@ -8,6 +8,8 @@ fish_add_path "$HOME/.local/bin"
 # default apps
 set -gx EDITOR nvim
 set -gx TERMINAL kitty
+set -gx TERMINAL_EXEC kitty
+set -gx BROWSER firefox
 
 # SSH agent
 set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
